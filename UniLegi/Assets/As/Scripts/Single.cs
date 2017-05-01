47fac41f636471c600b2e046983ca418a6f9faec
YamaTatsu8 <tatsu8fate@icloud.com>
2017-05-01 16:37:32 +0900

    弾の発射

---
+++ UniLegi/Assets/As/Scripts/Single.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Single : MonoBehaviour {
+
+    //弾のスピード
+    public float speed=5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(this.transform.position.y>5)
+        {
+            Destroy(this.gameObject);
+        }
+
+	}
+}
