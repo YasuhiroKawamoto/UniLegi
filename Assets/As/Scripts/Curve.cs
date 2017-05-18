61bb5432aea20171690e11d2a41b421c0e9c457a
YamaTatsu8 <tatsu8fate@icloud.com>
2017-05-18 16:51:51 +0900

    クールタイムの実装、ステータスからATK受け取る

--- Assets/As/Scripts/Curve.cs
+++ Assets/As/Scripts/Curve.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Curve : MonoBehaviour {
 
+    //拡大
     [SerializeField]
     private Vector3 m_scale;
     //始点
+    private Vector3 m_startPos;
     //終点
-
-
+    private Vector3 m_endPos;
+    //始点のベクトル
+    private Vector3 m_startVec;
+    //終点のベクトル
+    private Vector3 m_endVec;
+    //差分
+    [SerializeField]
+    private Vector3 m_pos = new Vector3(5.0f, 0, 0);
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -17,6 +26,18 @@ public class Curve : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.localScale += m_scale;
+        //transform.localScale += m_scale;
 	}
+
+    //スタート地点を設定する
+    public void setStartPos(Vector3 startPos)
+    {
+        m_startPos = startPos;
+    }
+    //終着点を設定する
+    public void setEndPos(Vector3 endPos)
+    {
+        m_endPos = endPos + m_pos;
+    }
+
 }
