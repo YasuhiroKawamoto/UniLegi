d2edbf0588415999e66edf68a48f9f3e829d29c2
slovenlyboy <slovenlyboy@gmail.com>
2017-05-28 15:25:29 +0900

    no message

--- Assets/23/Scripts/EnemyAttack.cs
+++ Assets/23/Scripts/EnemyAttack.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAttack : MonoBehaviour {
+public class EnemyAttack : MonoBehaviour
+{
 
     //Statesコンポーネント
     States states;
@@ -16,23 +17,24 @@ public class EnemyAttack : MonoBehaviour {
     [SerializeField]
     GameObject effect;
 
-   
 
-   private float rate;
 
-   private float cnt;
+    private float rate;
+
+    private float cnt;
 
     private bool AttackFlag;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
-       
- 
+
+
         states = this.gameObject.transform.parent.GetComponent<States>();
 
-      this.gameObject.transform.position =  new Vector3 (this.gameObject.transform.parent.position.x,
-          (this.gameObject.transform.parent.position.y-0.2f), this.gameObject.transform.parent.position.z);
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.parent.position.x,
+            (this.gameObject.transform.parent.position.y - 0.2f), this.gameObject.transform.parent.position.z);
 
         C = this.gameObject.GetComponent<Collider2D>();
 
@@ -44,65 +46,65 @@ public class EnemyAttack : MonoBehaviour {
 
         AttackFlag = false;
 
-       
+
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
         if (this.gameObject.GetComponentInParent<States>().GetDead() == false)
         {
 
 
             if (AttackFlag)//攻撃フラグがONであれば
+            {
+                states.SetCharge(cnt);
 
-            states.SetCharge(cnt);
 
+                cnt += Time.deltaTime;
+            }
             if (rate <= cnt)//攻撃間隔にカウントが到達
             {
-                cnt += Time.deltaTime;
 
-                if (rate <= cnt)//攻撃間隔にカウントが到達
+                if (target.gameObject.tag == "Player")//接触オブジェクトタグがPlayer
                 {
+                    target.GetComponent<States>().setDamege(states.getAttack());//ダメージ判定
 
-                    if (target.gameObject.tag == "Player")//接触オブジェクトタグがPlayer
-                    {
-                        target.GetComponent<States>().setDamege(states.getAttack());//ダメージ判定
-
-                        effect.transform.position = target.transform.position;//エフェクトの位置を設定
-                    }
-                    else if (target.gameObject.tag == "DangerZone")//接触オブジェクトタグがDangerZone
-                    {
-                        target.GetComponent<DangerZone>().SetHp(target.GetComponent<DangerZone>().GetHp() - states.getAttack());//ダメージ判定
-
-                        effect.transform.position = new Vector3(this.gameObject.transform.position.x, target.transform.position.y);//エフェクト位置設定
-                    }
-
-
-                    if (effect != null)//エフェクトスロットに設定してある場合
-                    {
-                        if (Singleton<SoundManager>.instance.getIsMute() == false)
-                        {
-                            Singleton<SoundManager>.instance.playSE("se001");
-                        }
-
-                        Instantiate(effect);//エフェクト生成
-                    }
-
-                    Debug.Log("攻撃");
-                    cnt = 0;//カウントリセット
+                    effect.transform.position = target.transform.position;//エフェクトの位置を設定
                 }
+                else if (target.gameObject.tag == "DangerZone")//接触オブジェクトタグがDangerZone
+                {
+                    target.GetComponent<DangerZone>().SetHp(target.GetComponent<DangerZone>().GetHp() - states.getAttack());//ダメージ判定
+
+                    effect.transform.position = new Vector3(this.gameObject.transform.position.x, target.transform.position.y);//エフェクト位置設定
+                }
+
+
+                if (effect != null)//エフェクトスロットに設定してある場合
+                {
+                    if (Singleton<SoundManager>.instance.getIsMute() == false)
+                    {
+                        Singleton<SoundManager>.instance.playSE("se001");
+                    }
+
+                    Instantiate(effect);//エフェクト生成
+                }
+
+                Debug.Log("攻撃");
+                cnt = 0;//カウントリセット
             }
         }
 
-       
+
+
     }
 
 
     void OnTriggerEnter2D(Collider2D col)
     {
 
-        
+
 
         if (col.gameObject.tag == "Player")//接触オブジェクトタグがPlayer
         {
@@ -143,7 +145,7 @@ public class EnemyAttack : MonoBehaviour {
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.tag == "DangerZone" || col.gameObject.tag == "Player" || col.gameObject.tag == "HavingPlayer")
-            {
+        {
             AttackFlag = false;//攻撃フラグOFF
             this.transform.parent.GetComponent<Mover>().setMoveFlag(true);//移動を開始
             Debug.Log("離脱");
