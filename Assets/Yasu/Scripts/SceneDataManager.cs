8d69f2d64b0967121ef393949229614c46c8b046
TSITCL\s162054 <yasu.suika@gmail.com>
2017-05-29 01:23:11 +0900

    合体修正

--- Assets/Yasu/Scripts/SceneDataManager.cs
+++ Assets/Yasu/Scripts/SceneDataManager.cs
@@ -19,30 +19,6 @@ public class SceneDataManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         fade.FadeOut(1);
-
-        if (SceneManager.GetActiveScene().name == "TitleScene")
-        {
-            Singleton<SoundManager>.instance.playBGM("BGM001", 0.0f);
-        }
-
-        if (SceneManager.GetActiveScene().name == "SelectScene")
-        {
-            Singleton<SoundManager>.instance.playBGM("BGM002", 0.0f);
-        }
-
-
-        if (SceneManager.GetActiveScene().name == "MainScene")
-        {
-            if (stageNumber == 1)
-            {
-                Singleton<SoundManager>.instance.playBGM("BGM003", 0.0f);
-            }
-            if (stageNumber == 2)
-            {
-                Singleton<SoundManager>.instance.playBGM("BGM001", 0.0f);
-            }
-        }
-
     }
 	
 	// Update is called once per frame
