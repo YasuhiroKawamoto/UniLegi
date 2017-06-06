e0a59c6e620376ec10d6a89e73fbd02cc8fa5ab4
TSITCL\s162054 <yasu.suika@gmail.com>
2017-06-06 13:43:08 +0900

    ユニット強さ視覚化

---
+++ Assets/Yasu/Scripts/StateUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StateUI : MonoBehaviour {
+
+    static public float HP;
+    static public float ATK;
+
+    Text hp;
+    Text atk;
+
+
+    // Use this for initialization
+    void Start () {
+        hp = GameObject.Find("HpTxt").GetComponent<Text>();
+        atk = GameObject.Find("AtkTxt").GetComponent<Text>();
+
+        hp.text = HP.ToString();
+        atk.text = ATK.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void SetHP(float hp)
+    {
+        HP = hp;
+    }
+
+    public void SetATK(float atk)
+    {
+        ATK = atk;
+    }
+}
