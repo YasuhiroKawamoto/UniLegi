f79591a3c6f7fa68b1751ddbaeaccf8765960e28
TSITCL\s162054 <yasu.suika@gmail.com>
2017-06-10 18:07:05 +0900

    komitto

--- Assets/Yasu/Scripts/Pause.cs
+++ Assets/Yasu/Scripts/Pause.cs
@@ -51,7 +51,7 @@ public class Pause : MonoBehaviour
                 m_flag = true;
             }
         }
-        else
+        else if(m_flag == true)
         {
             m_flag = false;
             Destroy(button.gameObject);
