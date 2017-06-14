3c7c26800ab48ed99e55634070435a6f47b88ac8
slovenlyboy <slovenlyboy@gmail.com>
2017-06-15 00:08:57 +0900

    サイドライン仮組

--- Assets/23/Scripts/SideLine.cs
+++ Assets/23/Scripts/SideLine.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SideLine : MonoBehaviour {
+public class SideLine : MonoBehaviour
+{
     //合体可能時に隣接ユニットを繋ぐライン生成スクリプト
 
     Collider2D CO;
@@ -10,14 +11,22 @@ public class SideLine : MonoBehaviour {
     [SerializeField]
     GameObject Line;//ユニット間に引くライン
 
-    GameObject LineObj = null;//ライン生成用オブジェ
+    GameObject LineObj;//ライン生成用オブジェ
 
     private GameObject nearObject;         //最も近いオブジェクト
     private float searchTime = 0;    //経過時間
 
+    GameObject Player;
+
+
     // Use this for initialization
     void Start()
     {
+        Player = GameObject.Find("Player");
+
+        Line = (GameObject)Resources.Load("Prefabs/Line");
+
+       
         //ユニットのリジッドボディを取得
         CO = this.GetComponent<Collider2D>();
         //最も近かったオブジェクトを取得
@@ -27,27 +36,43 @@ public class SideLine : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        //経過時間を取得
-        searchTime += Time.deltaTime;
+        //最も近かったオブジェクトを取得
+        nearObject = serchTag(gameObject, "Player");
+        //合体可能な場合
+        //if (Player.GetComponent<PlayerControl>().GetUnionCoolTime() == 0)
+        //{
 
-        if (searchTime >= 0.2f)
-        {
-            //最も近かったオブジェクトを取得
-            nearObject = serchTag(gameObject, "Player");
 
-            //経過時間を初期化
-            searchTime = 0;
-        }
+        ////経過時間を初期化
+        //searchTime = 0;
+
 
         if (nearObject != null)//最も近いオブジェクトがnullじゃなかったら
         {
-            if(LineObj == null)//ライン未精製時
-            //最も近いオブジェクト距離とのを取得して距離分の長さのラインを生成
-            LineObj.transform.position = (this.gameObject.transform.position - nearObject.transform.position);
-            //ラインの長さを決定
-            LineObj.transform.localScale = new Vector3(1,(this.gameObject.transform.position.x - nearObject.transform.position.x),1);
-            //ライン生成
-            LineObj = Instantiate(Line);
+            if (LineObj == null)//ライン未精製時 
+            {
+                //最も近いオブジェクト距離とのを取得して距離分の長さのラインを生成
+               
+
+                //ライン生成
+                LineObj = Instantiate(Line);
+
+                
+
+                if (this.gameObject.transform.position.x < nearObject.transform.position.x)
+                {
+                    LineObj.transform.position = new Vector3(this.gameObject.transform.position.x + 1f, this.gameObject.transform.position.y, 1);
+                }
+                else
+                {
+                    LineObj.transform.position = new Vector3(this.gameObject.transform.position.x - 1f, this.gameObject.transform.position.y, 1);
+                }
+
+
+                LineObj.transform.parent = transform;
+            }
+
+
         }
         else
         {
@@ -58,8 +83,8 @@ public class SideLine : MonoBehaviour {
                 Destroy(LineObj);
             }
         }
+        //}
     }
-
     //指定されたタグの中で最も近いものを取得
     GameObject serchTag(GameObject nowObj, string tagName)
     {
@@ -71,8 +96,10 @@ public class SideLine : MonoBehaviour {
         //タグ指定されたオブジェクトを配列で取得する
         foreach (GameObject obs in GameObject.FindGameObjectsWithTag(tagName))
         {
+
+
             //自身と取得したオブジェクトの距離を取得（y座標の差がユニットの縦幅より小さければ）
-            if ((Mathf.Abs(obs.transform.position.y - nowObj.transform.position.y) < CO.bounds.size.y))
+            if ((Mathf.Abs(obs.transform.position.y - nowObj.transform.position.y) < CO.bounds.size.y) && obs != this.gameObject)
             {
                 tmpDis = Vector3.Distance(obs.transform.position, nowObj.transform.position);
 
@@ -87,6 +114,9 @@ public class SideLine : MonoBehaviour {
             }
 
         }
+
+
+
         //最も近かったオブジェクトを返す
         return target;
     }
