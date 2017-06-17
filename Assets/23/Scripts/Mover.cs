fcb4179c58c33d7f14c72bc4570ebb30947d25c1
slovenlyboy <slovenlyboy@gmail.com>
2017-06-17 16:22:44 +0900

    敵前線動作調整

--- Assets/23/Scripts/Mover.cs
+++ Assets/23/Scripts/Mover.cs
@@ -52,160 +52,43 @@ public class Mover : MonoBehaviour
     {
         if (this.gameObject.GetComponent<States>().getDead() == false)
         {
-           
-            if (this.gameObject.GetComponent<States>().GetMoveType() == 5)//ランダム指定タイプだった場合
-            {
-                this.gameObject.GetComponent<States>().SetMoveType(Random.Range(1, 4));//移動タイプをランダムにする
-            }
+
+
 
 
 
             if (moveFlag == true)//フラグがtrueの時のみ動く
             {
+
+                walk.SetTrigger("Walk");
+
                 MC += Time.deltaTime;//経過時間取得
 
                 WaitEffectTime += Time.deltaTime;//経過時間取得
-
-                
-
-                if (MoveEffect != null)//エフェクトスロットに設定してある場合
-                {
-
-                    if (WaitEffectTime >= 0.0f)
-                    {
-                        MoveEffect.transform.position = this.gameObject.transform.position;//エフェクト位置指定 
-                        Instantiate(MoveEffect);//エフェクト生成
-
-                        WaitEffectTime = 0;
-                    }
-
-                }
 
 
                 if (this.gameObject.GetComponent<States>().GetMoveType() == 1)//移動タイプ１の場合
                 {
                     //速度加算
                     RB.velocity = Speed;
-                }
-                else if (this.gameObject.GetComponent<States>().GetMoveType() == 2)//移動タイプ2の場合
-                {
+
+
                    
-
-                    if (MC <= farstMoveTime)//第一指定時間に到達する前
-                    {
-
-                        RB.velocity = Speed; //速度加算
-                    }
-                    else//第一指定時間に到達後、第二指定時間到達まで
-                    {
-                        if (ReversFlag == false)
-                        {
-                            RB.velocity = Speed2; //速度加算
-                        }
-                        else
-                        {
-                            RB.velocity = (Vector2.Scale(Speed2, reverseX)); //速度加算
-                        }
-                    }
-
-
-                    if (MC >= secondMoveTime)//第2指定時間に到達
-                    {
-
-                        switch (ReversFlag)//反転フラグ反転
-                        {
-                            case true:
-                                ReversFlag = false;
-                                break;
-                            case false:
-                                ReversFlag = true;
-                                break;
-                        }
-
-                        MC = 0.0f;//カウントリセット
-                    }
-
                 }
-                else if (this.gameObject.GetComponent<States>().GetMoveType() == 3)//移動タイプ3の場合
-                {
 
-                    if (MC <= farstMoveTime)//第一指定時間に到達する前
-                    {
-
-                        RB.velocity = Speed; //速度加算
-
-                    }
-                    else//第一指定時間に到達後、第二指定時間到達まで
-                    {
-
-                        if (ReversFlag == false)
-                        {
-                            RB.velocity = Speed2; //速度加算
-                        }
-                        else
-                        {
-                            RB.velocity = (Vector2.Scale(Speed2, reverseX)); //速度加算
-                        }
-                    }
-
-                    if (MC >= secondMoveTime)//第2指定時間に到達
-                    {
-                        MC = 0.0f;//カウントリセット
-
-                        switch (ReversFlag)//反転フラグ反転
-                        {
-                            case true:
-                                ReversFlag = false;
-                                break;
-                            case false:
-                                ReversFlag = true;
-                                break;
-                        }
-                    }
-
-
-                }
-                else if (this.gameObject.GetComponent<States>().GetMoveType() == 4)//移動タイプ4の場合
-                {
-
-                    if (MC <= farstMoveTime)//第一指定時間に到達する前
-                    {
-
-                        RB.velocity = Speed;//速度加算
-
-                    }
-                    else//第一指定時間に到達後、第二指定時間到達まで
-                    {
-
-                        RB.velocity = Speed2;//速度加算
-                    }
-
-                    if (MC >= secondMoveTime)//第2指定時間に到達
-                    {
-                        MC = 0.0f;//カウントリセット
-                    }
-                }
-                else
-                {
-                    MC = 0.0f;//カウントリセット
-                }
+                
 
             }
             else if (moveFlag == false)//フラグがfalseなら動かない
             {
                 RB.velocity = new Vector3(0, 0, 0);//停止
                 walk.SetTrigger("Stop");
+
             }
 
+          
         }
-       
-        if(moveFlag==true)
-        {
-            walk.SetTrigger("Walk");
-        }
-
     }
-
     //移動フラグセット関数
     //引数（bool型　true or false）
     public void setMoveFlag(bool F)
